9808416f34cbf9a217bd72f46b019f29333b21dd
ekkuso <gm@free-ro.com>
2015-06-02 00:14:15 +0300

    Добавлен `Witch's Pumpkin Hat`
    Спасибо, Владимир!

--- files/item.js
+++ files/item.js
@@ -1778,6 +1778,7 @@ ItemOBJ = [
 ,[1769,52,0,0,0,0,10,0,"Watermelon Bite (FreeRO)",0,"",43,4,0]
 ,[1770,100,0,0,0,0,0,0,"Watermelon Set (FreeRO)",0,"",61,4,221,160,0]
 ,[1771,50,0,1,0,1,10,10,"Купальский венок (FreeRO)",0,"",6,4,154,5,153,5,0] 
+,[1772,50,0,0,0,1,70,0,"Witch's Pumpkin Hat (FreeRO)",0,"",19,10,193,1,56,15,67,10,221,79,0]
 ];
 
 //[ id, display location, job that can use, atk/def, level of weapon, slots, weight, level required to use, "name of item", ?, "description", effect1, effect2, ..., 0 =? ]
