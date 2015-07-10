00abab63d5784aba97e4ef9f94206472b442ff7d
ekkuso <gm@free-ro.com>
2015-07-10 14:38:52 +0300

    Добавлен `Butterfly Wing Ear`
    Спасибо, Владимир!

--- files/item.js
+++ files/item.js
@@ -1802,6 +1802,7 @@ ItemOBJ = [
 ,[1793,0,0,0,0,0,0,0,"Elven Set",0,"",25,100,0]
 ,[1794,51,0,1,0,0,40,70,"Stucked Bolts (FreeRO)",0,"",2,1,221,162,0]
 ,[1795,51,0,2,0,0,10,70,"Blood Butterfly Ears (FreeRO)",0,"",9,2,15,1,37,2,0]
+,[1796,51,0,0,0,0,10,70,"Butterfly Wing Ear (FreeRO)",0,"",5,1,0]
 ];
 
 //[ id, display location, job that can use, atk/def, level of weapon, slots, weight, level required to use, "name of item", ?, "description", effect1, effect2, ..., 0 =? ]
